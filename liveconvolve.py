d1e354bcce9a84ebec8f14959a5754d957e7f70a
Bill Warner <billwarner13@gmail.com>
2013-04-19 10:46:43 -0400

    kernel testing

--- liveconvolve.py
+++ liveconvolve.py
@@ -7,7 +7,7 @@ import numpy as np
 
 wc = CaptureFromCAM(0)
 NamedWindow('Camera',CV_WINDOW_AUTOSIZE)
-frames = 15 # approximate duration in seconds
+frames = 10 # approximate duration in seconds
 
 # Returns a CvMat object equivalent to entered list matrix
 def giveMeCV(l):
@@ -22,6 +22,11 @@ def giveMeCV(l):
 sobel = giveMeCV([[-1, 0, 1],
 			  	  [-2, 0, 2],
 			   	  [-1, 0, 1]])
+test = np.matrix([[-1, 0, 1],
+			  	  [-2, 0, 2],
+			   	  [-1, 0, 1]])
+test = np.transpose(test)
+sobelT = giveMeCV(test.tolist())
 
 laplace = giveMeCV([[-1,-1,-1],
 					[-1, 8,-1],
@@ -34,7 +39,7 @@ uberlaplace = giveMeCV([[-1,-1,-1,-1,-1],
 						[-1,-1,-1,-1,-1]])
 
 # Applied Kernel
-kernel = uberlaplace
+kernel = sobel
 
 start = time() # for framerate checking (usu. ~30)
 for i in range(frames*30):
