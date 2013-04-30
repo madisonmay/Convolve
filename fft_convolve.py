2b9158d905b3b3b1e712e88d3a9b837cf74544b6
madisonmay13 <madison.may@students.olin.edu>
2013-04-30 18:52:50 -0400

    Changes to fft_convole

--- fft_convolve.py
+++ fft_convolve.py
@@ -2,19 +2,27 @@ from PIL import Image
 import numpy as np
 from numpy import *
 from scipy import misc
+import PIL.ImageOps
 
 def convolve_fft(img, kernel):
 
     #Dimensions of img -- rows/cols
     image_rows, image_cols = img.shape
 
-
     fft_img = np.fft.fft2(img, s=(image_rows, image_cols))
+
+    fft_img = np.roll(fft_img, image_rows/2, axis=0)
+    fft_img = np.roll(fft_img, image_cols/2, axis=1)
+
     fft_kernel = np.fft.fft2(kernel, s=(image_rows, image_cols))
+
+    fft_kernel = np.roll(fft_kernel, image_rows/2, axis=0)
+    fft_kernel = np.roll(fft_kernel, image_cols/2, axis=1)
+
     fft_image = fft_img * fft_kernel
     img_out = np.fft.ifft2(fft_image)
 
-    return img_out
+    return fft_img
 
 
 
@@ -45,16 +53,25 @@ def convolve_wrapper(imagename, savename):
 
     weights = [.2126, .7152, .0722]
     for i in range(channels): #scale each color channel to between 0 and 255
-        img_channel = img_out[:,:,i]
+        img_channel = abs(img_out[:,:,i])
         channel_min = img_channel.min()
         img_channel = img_channel- channel_min
         channel_max = img_channel.max()
         img_channel = (img_channel/channel_max)*255.0
         img_out[:,:,i] = img_channel*weights[i]
 
+    def mappoint (i):
+        if i > 0: return 255
+        else: return 0
+
     img = img_out[:,:,0] + img_out[:,:,1] + img_out[:,:,2]
     misc.imsave(savename, img.real)
+
+    img = Image.open('fft.png')
+    img = img.point(mappoint)
+    img.save('fft.png')
+
     return img_unscaled, img_out
 
 if __name__ == '__main__':
-    convolve_wrapper('miller.jpg', 'fft.png')
+    convolve_wrapper('cityscape.jpg', 'fft.png')
